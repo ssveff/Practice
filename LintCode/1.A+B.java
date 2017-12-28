14b58cd036e1a7eafd4a9201310404de1a952c89
ssveff <aoranwang06@hotmail.com>
2017-12-28 17:16:04 +0800

    First Practice

---
+++ LintCode/1.A+B.java
@@ -0,0 +1,10 @@
+public class Solution {
+    /*
+     * @param : An integer
+     * @param : An integer
+     * @return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // write your code here
+        return (a & b << 1) + a ^ b;
+    }
