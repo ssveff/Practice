2dff707a2e5897f8e975ddc225442cd875aeabdf
ssveff <aoranwang06@hotmail.com>
2017-12-30 10:59:53 +0800

    Stack & Queue

---
+++ LintCode/423.ValidParentheses.java
@@ -0,0 +1,32 @@
+public class Solution {
+    /*
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        // write your code here
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (char c: s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+        }
+    }
---
+++ LintCode/485.GenerateArrayListWithGivenSize.java
@@ -0,0 +1,14 @@
+public class Solution {
+    /*
+     * @param size: An integer
+     * @return: An integer list
+     */
+    public List<Integer> generate(int size) {
+        // write your code here
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 1; i <= size; i++){
+            result.add(i);
+        }
+        return result;
+    }
+}
