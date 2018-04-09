b5dc8917a6aba023b9f9cc3e4a2f233e9f0671a6
Aoran Wang <aoranw@andrew.cmu.edu>
2018-04-09 03:40:04 -0400

    0408 Push

---
+++ LeetCode/771.JewelsAndStones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> Jset = new HashSet();
+        for(char j: J.toCharArray()){
+            Jset.add(j);
+        }
+        int count = 0;
+        for(char s: S.toCharArray()){
+            if(Jset.contains(s)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
